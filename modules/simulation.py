bd6f83926927b2e225d589f2f41f712689ca27af
Tringin <lorentaj@outlook.com>
2025-06-28 16:57:46 +0200

    trying out some investment caluclations

--- modules/simulation.py
+++ modules/simulation.py
@@ -1,4 +1,13 @@
-def calculate_investments(
-    initial_capital, recurring_capital, rhythm, timeframe, return_rate
-):
-    pass
+from modules.fees import get_transaction_fees
+
+invested_capital = []
+
+def calculate_investments(initial_capital, recurring_capital, rhythm, timeframe, return_rate):
+    if initial_capital != 0:
+        invested_capital = initial_capital
+        invested_capital - get_transaction_fees(initial_capital)
+        return invested_capital
+    else:
+        invested_capital = invested_capital + recurring_capital
+        invested_capital - get_transaction_fees(invested_capital)
+        return invested_capital
