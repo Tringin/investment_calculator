c7f90b64946bc86f3ab50373299a0a92131e4df1
Tringin <lorentaj@outlook.com>
2025-06-28 15:03:47 +0200

    Gui 'recurring capital' and 'initial capital' '

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-import tkinter
+import tkinter as tk
 from decimal import Decimal
+
 import matplotlib.pyplot as plt
 
-#from modules.fees import FUNKTIONEN
+from modules.fees import get_transaction_fees
 #from modules.simulation import FUNKTIONEN
 
+root = tk.Tk()
+root.geometry("800x600")
+root.title("Investment calculator")
+
+#text for recurring capital
+tk.Label(root, text="Recurring capital").grid(row=0, column=0)
+
+#creating entry field and saving it in the variable. ofc also layout
+recurring_capital = tk.Entry(root)
+recurring_capital.grid(row=0, column=1)
+
+#text for starting capital
+tk.Label(root, text="Initial capital").grid(row=0, column=2)
+
+#creating entry field and saving it in the variable. ofc also layout
+initial_capital = tk.Entry(root)
+initial_capital.grid(row=0, column=3)
+
+def get_capital():
+    amount = initial_capital.get()
+    amount = Decimal(amount)
+    value = get_transaction_fees(amount)
+    output_label.config(text=f"Gebühren: CHF {value}")
+
+
+#runs the main window loop
+root.mainloop()
--- modules/fees.py
+++ modules/fees.py
@@ -1,14 +1,30 @@
 from decimal import Decimal
 
 transaction_fees = {
-    (Decimal("0"), Decimal("500.00")): 3,
+    (Decimal("0.00"), Decimal("500.00")): 3,
     (Decimal("500.01"), Decimal("1000.00")): 5,
     (Decimal("1000.01"), Decimal("2000.00")): 10,
     (Decimal("2000.01"), Decimal("10000.00")): 30,
     (Decimal("10000.01"), Decimal("15000.00")): 55,
     (Decimal("15000.01"), Decimal("25000.00")): 80,
     (Decimal("25000.01"), Decimal("50000.00")): 135,
-    (Decimal("50000.01"), Decimal("2000.00")): 190,
+    (Decimal("50000.01"), Decimal("inf")): 190,
 }
 
 def get_transaction_fees(amount):
+    amount = Decimal(str(amount))
+    for key, value in transaction_fees.items():
+        if amount >= key[0] and amount <= key[1]:
+            return value
+
+etf_leader_fees = {
+    (Decimal("0.00"), Decimal("500.00")): 3,
+    (Decimal("500.01"), Decimal("1000.00")): 5,
+    (Decimal("1000.01"), Decimal("inf")): 9,
+}
+
+def get_leader_fees(amount):
+    amount = Decimal(str(amount))
+    for key, value in etf_leader_fees.items():
+        if amount >= key[0] and amount <= key[1]:
+            return value
