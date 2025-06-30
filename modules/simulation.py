08f580b6103730ce8452c987711840830f577b85
Tringin <lorentaj@outlook.com>
2025-06-30 18:44:16 +0200

    ..

--- modules/simulation.py
+++ modules/simulation.py
@@ -7,6 +7,7 @@ def calculate_investments(initial_capital, recurring_capital, rhythm, timeframe,
     recurring_capital = Decimal(str(recurring_capital))
     return_rate = Decimal(str(return_rate))
 
-    for months in range(1, timeframe, +1):
-        if months % 1:
-            portfolio_values = portfolio_values + recurring_capital
+    if initial_capital > 0:
+        for months in range(1, timeframe, +1):
+            if months % 1:
+                portfolio_values = portfolio_values + recurring_capital
