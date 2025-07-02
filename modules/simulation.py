32c2ba2986502fedbf1b5769c289a4ae8258d6fc
Tringin <lorentaj@outlook.com>
2025-07-02 16:37:18 +0200

    ..

--- modules/simulation.py
+++ modules/simulation.py
@@ -8,14 +8,26 @@ def calculate_investments(initial_capital, recurring_capital, rhythm, timeframe,
     initial_capital = Decimal(str(initial_capital))
     recurring_capital = Decimal(str(recurring_capital))
     return_rate = Decimal(str(return_rate))
+    timeframe = int(timeframe)
 
     if initial_capital != 0:
-        portfolio_values = initial_capital
+        portfolio_values = initial_capital + recurring_capital
+        portfolio_values -= get_transaction_fees(portfolio_values)
         for months in range(1, timeframe + 1):
             if months % RHYTHM_MAPPING[rhythm] == 0:
                 recurring_capital -= get_transaction_fees(recurring_capital)
                 portfolio_values += recurring_capital
-                if months != 1:
+        for months in range(1, timeframe + 1):
+            monthly_return = return_rate / timeframe
+            portfolio_values = portfolio_values * (1 + monthly_return)
+
+    else:
+        initial_capital -= get_transaction_fees(initial_capital)
+        portfolio_values += initial_capital
+        for months in range(1, timeframe + 1):
+            if months % RHYTHM_MAPPING[rhythm] == 0:
+                recurring_capital -= get_transaction_fees(recurring_capital)
+                portfolio_values += recurring_capital
+                for months in range(1, timeframe + 1):
                     monthly_return = return_rate / timeframe
                     portfolio_values = portfolio_values * (1 + monthly_return)
-    return portfolio_values
